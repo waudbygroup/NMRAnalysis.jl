6df735f5dd8d5072cf004c2ad25a9f0056553824
chriswaudby <cwaudby@gmail.com>
2023-12-07 17:32:05 +0000

    add JuliaFormatter

--- src/diffusion.jl
+++ src/diffusion.jl
@@ -6,15 +6,14 @@ function analyse_diffusion(coherence=SQ(H1))
     experiment = readline()
     ispath(experiment) || throw(SystemError("No such file or directory"))
 
-    analyse_diffusion(experiment, coherence)
+    return analyse_diffusion(experiment, coherence)
 end
-
 
 function analyse_diffusion(experiment::String, coherence=SQ(H1))
-    analyse_diffusion(loadnmr(experiment), coherence)
+    return analyse_diffusion(loadnmr(experiment), coherence)
 end
 
-function analyse_diffusion(spec::NMRData{T,2}, coherence=SQ(H1)) where T
+function analyse_diffusion(spec::NMRData{T,2}, coherence=SQ(H1)) where {T}
     spec = deepcopy(spec) # work on copy of the data
     label!(spec, "Diffusion")
 
@@ -24,12 +23,12 @@ function analyse_diffusion(spec::NMRData{T,2}, coherence=SQ(H1)) where T
     println("Parsing experiment parameters...")
     γ = gyromagneticratio(coherence)
     δ = acqus(spec, :p, 30) * 2e-6  # gradient pulse length
-	Δ = acqus(spec, :d, 20)         # diffusion delay
+    Δ = acqus(spec, :d, 20)         # diffusion delay
     gpnam = acqus(spec, :gpnam, 6)  # try to identify shape factor
     σ = 1
     if length(gpnam) ≥ 4
         if gpnam[1:4] == "SMSQ"
-	        σ = 0.9
+            σ = 0.9
         elseif gpnam[1:4] == "SINE"
             σ = 0.6366
         end
@@ -37,33 +36,33 @@ function analyse_diffusion(spec::NMRData{T,2}, coherence=SQ(H1)) where T
 
     print("Gradient pulse length δ = $(1e6*δ) μs (2*p30). Press enter to confirm or type correct value (in μs): ")
     response = readline()
-    if length(response)>0
+    if length(response) > 0
         δ = tryparse(Float64, response) * 1e-6
     end
 
     print("Diffusion delay Δ = $Δ s (d20). Press enter to confirm or type correct value (in s): ")
     response = readline()
-    if length(response)>0
+    if length(response) > 0
         Δ = tryparse(Float64, response)
     end
 
     print("Gradient shape factor σ = $σ (gpnam6 = $gpnam). Press enter to confirm or type correct value: ")
     response = readline()
-    if length(response)>0
+    if length(response) > 0
         σ = tryparse(Float64, response)
     end
 
     Gmax = 0.55
     print("Max. gradient strength Gmax = $Gmax T m⁻¹ (typical value for Bruker systems). Press enter to confirm or type correct value (in T m⁻¹): ")
     response = readline()
-    if length(response)>0
+    if length(response) > 0
         Gmax = tryparse(Float64, response)
     end
-    
+
     print("Enter initial gradient strength (%): ")
     response = readline()
     g1 = tryparse(Float64, response) * 0.01
-    
+
     print("Enter final gradient strength (%): ")
     response = readline()
     g2 = tryparse(Float64, response) * 0.01
@@ -81,29 +80,28 @@ function analyse_diffusion(spec::NMRData{T,2}, coherence=SQ(H1)) where T
     else
         throw(ArgumentError("Invalid input"))
     end
-    
+
     spec = setgradientlist(spec, g, Gmax)
 
     # 2. pick integration region
-    plt = plot(spec[:,1], grid=true)
-    hline!(plt, [0], c=:grey)
+    plt = plot(spec[:, 1]; grid=true)
+    hline!(plt, [0]; c=:grey)
     display(plt)
-    
+
     print("Defining integration region - please enter first chemical shift: ")
     ppm1 = readline()
     ppm1 = tryparse(Float64, ppm1)
     print("Defining integration region - please enter second chemical shift: ")
     ppm2 = readline()
     ppm2 = tryparse(Float64, ppm2)
-    
+
     # 3. pick noise region
-    vspan!(plt, [ppm1, ppm2], label="integration region", alpha=0.2)
+    vspan!(plt, [ppm1, ppm2]; label="integration region", alpha=0.2)
     display(plt)
 
     print("Enter a chemical shift in the center of the noise region: ")
     noiseppm = readline()
     noiseppm = tryparse(Float64, noiseppm)
-
 
     # create integration region and noise selectors
     ppm1, ppm2 = minmax(ppm1, ppm2)
@@ -116,50 +114,50 @@ function analyse_diffusion(spec::NMRData{T,2}, coherence=SQ(H1)) where T
     noiseroi = noise1 .. noise2
 
     # plot region
-    p2 = plot(spec[:,1],linecolor=:black)
-    hline!(p2, [0], c=:grey, primary=false)
-    plot!(p2, spec[roi,1], fill=(0,:dodgerblue), linecolor=:navy, label="integration region")
-    plot!(p2, spec[noiseroi,1], fill=(0,:orange), linecolor=:red, label="noise region", legend=:topright)
+    p2 = plot(spec[:, 1]; linecolor=:black)
+    hline!(p2, [0]; c=:grey, primary=false)
+    plot!(p2, spec[roi, 1]; fill=(0, :dodgerblue), linecolor=:navy,
+          label="integration region")
+    plot!(p2, spec[noiseroi, 1]; fill=(0, :orange), linecolor=:red, label="noise region",
+          legend=:topright)
     title!(p2, "Integration regions")
     display(p2)
     print("Displaying integration and noise regions. Press enter to continue.")
     readline()
-    
 
     # 4. integrate regions
-    noise = vec(data(sum(spec[noiseroi,:], dims=F1Dim)))
+    noise = vec(data(sum(spec[noiseroi, :]; dims=F1Dim)))
     noise = std(noise)
-	
-	integrals = vec(data(sum(spec[roi,:],dims=F1Dim)))
-	
-	# normalise by max value
-	noise /= maximum(integrals)
+
+    integrals = vec(data(sum(spec[roi, :]; dims=F1Dim)))
+
+    # normalise by max value
+    noise /= maximum(integrals)
     integrals /= maximum(integrals)
-    
+
     # 5. fit region
-    model(g, p) = p[1] * exp.(-(γ*δ*σ*g*Gmax).^2 .* (Δ - δ/3) .* p[2] .* 1e-10)
+    model(g, p) = p[1] * exp.(-(γ * δ * σ * g * Gmax) .^ 2 .* (Δ - δ / 3) .* p[2] .* 1e-10)
 
     p0 = [1.0, 1.0] # initial guesses for amplitude, D
     fit = curve_fit(model, g, integrals, p0)
     pars = coef(fit) .± stderror(fit)
     I0 = coef(fit)[1]
     D = pars[2] * 1e-10
-    
-    
+
     # 7. plot fitted region
     maxg = maximum(g)
-    x = LinRange(0, maxg*1.1, 100)
+    x = LinRange(0, maxg * 1.1, 100)
     yfit = model(x, coef(fit))
 
-    p1 = scatter(g*Gmax, (integrals .± noise) / I0, label="observed",
-			frame=:box,
-			xlabel="Gradient strength / T m⁻¹",
-			ylabel="Integrated signal",
-			title="Diffusion: D = $D m² s⁻¹",
-			grid=nothing)
-    plot!(p1, x*Gmax, yfit / I0,
-        label="fit",
-        z_order = :back)
+    p1 = scatter(g * Gmax, (integrals .± noise) / I0; label="observed",
+                 frame=:box,
+                 xlabel="Gradient strength / T m⁻¹",
+                 ylabel="Integrated signal",
+                 title="Diffusion: D = $D m² s⁻¹",
+                 grid=nothing)
+    plot!(p1, x * Gmax, yfit / I0;
+          label="fit",
+          z_order=:back)
 
     println()
     @info """diffusion results
@@ -180,7 +178,7 @@ Fitted diffusion coefficient: $D m² s⁻¹
     print("Enter a filename to save figure (press enter to skip): ")
     outputfilename = readline()
     if length(outputfilename) > 0
-        plot!(p1, title="")
+        plot!(p1; title="")
         savefig(p1, outputfilename)
         println("Figure saved to $outputfilename.")
     end
--- src/tract.jl
+++ src/tract.jl
@@ -10,18 +10,15 @@ function analyse_tract()
     ispath(trosy) || throw(SystemError("No such file or directory"))
     antitrosy = readline()
 
-    analyse_tract(trosy, antitrosy)
+    return analyse_tract(trosy, antitrosy)
 end
-
 
 function analyse_tract(trosy::String, antitrosy::String)
-    analyse_tract(
-        loadnmr(trosy),
-        loadnmr(antitrosy)
-    )
+    return analyse_tract(loadnmr(trosy),
+                         loadnmr(antitrosy))
 end
 
-function analyse_tract(trosy::NMRData{T,2}, antitrosy::NMRData{T,2}) where T
+function analyse_tract(trosy::NMRData{T,2}, antitrosy::NMRData{T,2}) where {T}
     # numerical constants
     μ0 = 4π * 1e-7
     γH = gyromagneticratio(H1)
@@ -47,33 +44,30 @@ function analyse_tract(trosy::NMRData{T,2}, antitrosy::NMRData{T,2}) where T
     ωN = 2π * 1e6 * acqus(trosy, :bf3)
 
     p = μ0 * γH * γN * ħ / (8π * sqrt(2) * rNH^3)
-    c = B0 * γN * ΔδN / (3*sqrt(2))
+    c = B0 * γN * ΔδN / (3 * sqrt(2))
     f = p * c * (3cos(θ)^2 - 1)
-    
+
     # 2. pick integration region
 
-    plt = plot([
-        trosy[:,1],
-        antitrosy[:,1]
-    ], grid=true)
-    hline!(plt, [0], c=:grey)
+    plt = plot([trosy[:, 1],
+                antitrosy[:, 1]]; grid=true)
+    hline!(plt, [0]; c=:grey)
     display(plt)
-    
+
     print("Defining integration region - please enter first chemical shift: ")
     ppm1 = readline()
     ppm1 = tryparse(Float64, ppm1)
     print("Defining integration region - please enter second chemical shift: ")
     ppm2 = readline()
     ppm2 = tryparse(Float64, ppm2)
-    
+
     # 3. pick noise region
-    vspan!(plt, [ppm1, ppm2], label="integration region", alpha=0.2)
+    vspan!(plt, [ppm1, ppm2]; label="integration region", alpha=0.2)
     display(plt)
 
     print("Enter a chemical shift in the center of the noise region: ")
     noiseppm = readline()
     noiseppm = tryparse(Float64, noiseppm)
-
 
     # create integration region and noise selectors
     ppm1, ppm2 = minmax(ppm1, ppm2)
@@ -86,32 +80,32 @@ function analyse_tract(trosy::NMRData{T,2}, antitrosy::NMRData{T,2}) where T
     noiseroi = noise1 .. noise2
 
     # plot region
-    p2 = plot(trosy[:,1],linecolor=:black)
-    hline!(p2, [0], c=:grey, primary=false)
-    plot!(p2, trosy[roi,1], fill=(0,:dodgerblue), linecolor=:navy, label="integration region")
-    plot!(p2, trosy[noiseroi,1], fill=(0,:orange), linecolor=:red, label="noise region", legend=:topright)
+    p2 = plot(trosy[:, 1]; linecolor=:black)
+    hline!(p2, [0]; c=:grey, primary=false)
+    plot!(p2, trosy[roi, 1]; fill=(0, :dodgerblue), linecolor=:navy,
+          label="integration region")
+    plot!(p2, trosy[noiseroi, 1]; fill=(0, :orange), linecolor=:red, label="noise region",
+          legend=:topright)
     title!(p2, "Integration regions")
     display(p2)
     print("Displaying integration and noise regions. Press enter to continue.")
     readline()
-    
 
     # 4. integrate regions
-    trosynoise = vec(data(sum(trosy[noiseroi,:], dims=F1Dim)))
-    antitrosynoise = vec(data(sum(antitrosy[noiseroi,:], dims=F1Dim)))
-	trosynoise = std(trosynoise)
-	antitrosynoise = std(antitrosynoise)
-	
-	trosyintegrals = vec(data(sum(trosy[roi,:],dims=F1Dim)))
-    antitrosyintegrals = vec(data(sum(antitrosy[roi,:],dims=F1Dim)))
-	
-	# normalise by max value
-	trosynoise /= maximum(trosyintegrals)
+    trosynoise = vec(data(sum(trosy[noiseroi, :]; dims=F1Dim)))
+    antitrosynoise = vec(data(sum(antitrosy[noiseroi, :]; dims=F1Dim)))
+    trosynoise = std(trosynoise)
+    antitrosynoise = std(antitrosynoise)
+
+    trosyintegrals = vec(data(sum(trosy[roi, :]; dims=F1Dim)))
+    antitrosyintegrals = vec(data(sum(antitrosy[roi, :]; dims=F1Dim)))
+
+    # normalise by max value
+    trosynoise /= maximum(trosyintegrals)
     antitrosynoise /= maximum(antitrosyintegrals)
 
     trosyintegrals /= maximum(trosyintegrals)
     antitrosyintegrals /= maximum(antitrosyintegrals)
-
 
     # 5. fit region
     model(t, p) = p[1] * exp.(-p[2] * t)
@@ -131,34 +125,36 @@ function analyse_tract(trosy::NMRData{T,2}, antitrosy::NMRData{T,2}) where T
 
     # based on analytical solution in Mathematica
     # Solve[f (4*4/10*tc + (3*4/10*tc/(1 + (tc*\[Omega]N)^2))) == \[Eta], tc]
-    x2 = 21952*f^6*ωN^6 - 3025*f^4*ηxy^2*ωN^8 + 625*f^2*ηxy^4*ωN^10
+    x2 = 21952 * f^6 * ωN^6 - 3025 * f^4 * ηxy^2 * ωN^8 + 625 * f^2 * ηxy^4 * ωN^10
     x = sqrt(x2)
-    y3 = 1800*f^2*ηxy*ωN^4 + 125*ηxy^3*ωN^6 + 24*sqrt(3)*x
+    y3 = 1800 * f^2 * ηxy * ωN^4 + 125 * ηxy^3 * ωN^6 + 24 * sqrt(3) * x
     y = cbrt(y3)
-    τc = (5*ηxy)/(24*f) - (336*f^2*ωN^2 - 25*ηxy^2*ωN^4)/(24*f*ωN^2*y) + y/(24*f*ωN^2)
+    τc = (5 * ηxy) / (24 * f) -
+         (336 * f^2 * ωN^2 - 25 * ηxy^2 * ωN^4) / (24 * f * ωN^2 * y) + y / (24 * f * ωN^2)
     τc = 1e9 * τc
-    
+
     # 7. plot fitted region
     maxt = max(maximum(trosytau), maximum(antitrosytau))
-    t = LinRange(0, maxt*1.1, 100)
+    t = LinRange(0, maxt * 1.1, 100)
     yfit1 = model(t, coef(trosyfit))
     yfit2 = model(t, coef(antitrosyfit))
 
-    p1 = scatter(1000trosytau, (trosyintegrals .± trosynoise) / trosyI0, label="TROSY",
-			frame=:box,
-			xlabel="Relaxation time / ms",
-			ylabel="Integrated signal",
-			title="TRACT: τc = $τc ns",
-			grid=nothing)
-    scatter!(p1, 1000antitrosytau, (antitrosyintegrals .± antitrosynoise) / antitrosyI0, label="Anti-TROSY")
-    plot!(p1, 1000t, yfit1 / trosyI0,
-        label="TROSY fit (R₂ = $trosyR2 s⁻¹)",
-        c=1,
-        z_order = :back)
-    plot!(p1, 1000t, yfit2 / antitrosyI0,
-        label="Anti-TROSY fit (R₂ = $antitrosyR2 s⁻¹)",
-        c=2,
-        z_order = :back)
+    p1 = scatter(1000trosytau, (trosyintegrals .± trosynoise) / trosyI0; label="TROSY",
+                 frame=:box,
+                 xlabel="Relaxation time / ms",
+                 ylabel="Integrated signal",
+                 title="TRACT: τc = $τc ns",
+                 grid=nothing)
+    scatter!(p1, 1000antitrosytau, (antitrosyintegrals .± antitrosynoise) / antitrosyI0;
+             label="Anti-TROSY")
+    plot!(p1, 1000t, yfit1 / trosyI0;
+          label="TROSY fit (R₂ = $trosyR2 s⁻¹)",
+          c=1,
+          z_order=:back)
+    plot!(p1, 1000t, yfit2 / antitrosyI0;
+          label="Anti-TROSY fit (R₂ = $antitrosyR2 s⁻¹)",
+          c=2,
+          z_order=:back)
 
     println()
     @info """TRACT results
@@ -185,7 +181,7 @@ N.B. TRACT analysis assumes the protein is perfectly rigid. In the presence of f
     print("Enter a filename to save figure (press enter to skip): ")
     outputfilename = readline()
     if length(outputfilename) > 0
-        plot!(p1, title="")
+        plot!(p1; title="")
         savefig(p1, outputfilename)
         println("Figure saved to $outputfilename.")
     end
