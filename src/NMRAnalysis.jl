5d77089458a5289be7c5c8db1e7d4bec098eecba
chriswaudby <cwaudby@gmail.com>
2023-12-07 17:16:15 +0000

    add diffusion analysis

--- src/NMRAnalysis.jl
+++ src/NMRAnalysis.jl
@@ -7,25 +7,28 @@ using Plots
 using Roots
 using Statistics
 
-# include("1d_fitting.jl")
+include("diffusion.jl")
 include("tract.jl")
 
 using PrecompileTools
 include("precompile.jl")
 
 export analyse_tract
+export analyse_diffusion
 
 @info """
 NMRAnalysis.jl
-==============
 
 Usage:
-* set your working directory to a convenient location, e.g.
+- set your working directory to a convenient location, e.g.
   cd("/Users/chris/NMR/crick-702/my_experiment_directory")
-* call the desired analysis routine with the path to the experiment directories
+- call the desired analysis routine
 
 Available analysis routines:
-* analyse_tract(trosy_filename, antitrosy_filename)
+- analyse_tract()
+- analyse_diffusion()
+
+Current working directory: $(pwd())
 """
 
 end
