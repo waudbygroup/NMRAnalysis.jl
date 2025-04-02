495fb5bc9b98cbdadfcac480f6f24a64e2d21007
chriswaudby <cwaudby@gmail.com>
2025-04-02 13:16:24 +0100

    minor fixes

--- src/R1rho/calibrations.jl
+++ src/R1rho/calibrations.jl
@@ -2,45 +2,44 @@ function setupR1rhopowers()
     # ANSI escape code for magenta
     magenta = "\033[35m"
     reset = "\033[0m"
-    
+
     # Get the terminal width
     term_width = displaysize(stdout)[2]
     line_break = repeat("-", term_width)
-    
+
     # Prompt the user for inputs with magenta color and caret
     println()
-    println("$magenta Enter pldb1:$reset")
-    print("> ")
-    pldb1 = parse(Float64, readline())
-    
-    println()
-    println("$magenta Enter p1 (in us):$reset")
+    println("Enter p1 (19F hard pulse power, in us):")
     print("> ")
     p1 = parse(Float64, readline()) * 1e-6 # convert to seconds
-    
+
     println()
-    println("$magenta Input your own list of target spinlock strengths (in Hz) separated by commas, or press enter for a default list:$reset")
+    println("Enter pldB1 (19F hard pulse power, in dB):")
+    print("> ")
+    pldb1 = parse(Float64, readline())
+
     println()
-    println("The default spinlock strengths are: [100, 200, 300, 500, 750, 1000, 1500, 2000, 3000, 4000, 5000, 6000, 7000, 8000, 9000, 10000, 11000, 12000, 13000, 14000, 15000]")
-    println()
+    println("Input a list of spinlock strengths (in Hz) separated by commas, or press ENTER for a default list:")
     print("> ")
     input = readline()
-    
+
     if input == ""
-        target_spinlock_strengths = [100, 200, 300, 500, 750, 1000, 1500, 2000, 3000, 4000, 5000, 6000, 7000, 8000, 9000, 10000, 11000, 12000, 13000, 14000, 15000]
+        target_spinlock_strengths = [100, 200, 300, 500, 750, 1000, 1500, 2000, 3000, 4000,
+                                     5000, 6000, 7000, 8000, 9000, 10000, 11000, 12000,
+                                     13000, 14000, 15000]
+        println("Using default spinlock strengths (in Hz):\n[100, 200, 300, 500, 750, 1000, 1500, 2000, 3000, 4000, 5000, 6000, 7000, 8000, 9000, 10000, 11000, 12000, 13000, 14000, 15000]")
     else
-        target_spinlock_strengths = parse.(Float64, split(input, ","))
+        target_spinlock_strengths = parse.(Float64, strip.(split(input, ",")))
     end
-    
-        # Check for high spinlock strengths
-    high_spinlock_strength = 13000
-    max_spinlock_strength = 15000
-    if any(x -> x > high_spinlock_strength, target_spinlock_strengths)
+
+    # Check for high spinlock strengths (above 10 kHz)
+    max_spinlock_strength = maximum(target_spinlock_strengths)
+    if max_spinlock_strength > 10000
         println()
-        println("$magenta WARNING - high spin-lock powers may cause damage to your probe! 
- Check association spin-lock duration is within acceptable power limits. 
- Maximum spin-lock strength will be $max_spinlock_strength Hz. 
- Type 'yes' to proceed. Do you want to proceed? (yes/no):$reset")
+        println("$(magenta)WARNING - high spin-lock powers may cause damage to your probe! 
+Check the spin-lock duration is within acceptable power limits. 
+Maximum spin-lock strength will be $max_spinlock_strength Hz. 
+Type 'yes' to proceed. Do you want to proceed? (yes/no):$reset")
         println()
         print("> ")
         confirmation = readline()
@@ -52,15 +51,16 @@ function setupR1rhopowers()
 
     # Calculate the final powers
     final_powers = convert_Hz_to_dB.(target_spinlock_strengths, pldb1, p1)
-    
+
     # Shuffle the final powers list and the corresponding spinlock strengths
     shuffled_indices = shuffle(1:length(final_powers))
     shuffled_final_powers = final_powers[shuffled_indices]
     shuffled_spinlock_strengths = target_spinlock_strengths[shuffled_indices]
-    
+
     # Print the final powers in the specified format
     println()
-    println("The list corresponds to the following spinlock strengths (Hz): ", shuffled_spinlock_strengths)
+    println("The list corresponds to the following spinlock strengths (Hz):\n",
+            shuffled_spinlock_strengths)
     println()
     println("Copy & paste the list provided between the dashed lines.")
     println(line_break)
@@ -68,5 +68,5 @@ function setupR1rhopowers()
     for power in shuffled_final_powers
         println(@sprintf("%.2f", power))
     end
-    println(line_break)
+    return println(line_break)
 end
